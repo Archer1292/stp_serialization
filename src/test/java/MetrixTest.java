0c0cb4681ce6e61fae9956c6b342509d3c086ee0
Archer1292 <archer1292@gmail.com>
2017-12-08 22:26:56 +0200

    +metrix test

---
+++ src/test/java/MetrixTest.java
@@ -0,0 +1,14 @@
+/**
+ * @author Bratus Nadja on 12/8/2017
+ * @project lab5
+ */
+public class MetrixTest {
+
+    public MetrixTest() { /* compiled code */ }
+
+    @org.junit.Test
+    public void testMetrixTime() { /* compiled code */ }
+
+    @org.junit.Test
+    public void testMetrixMemory() { /* compiled code */ }
+}
